f348970b160038e1de18296adaf2caf161fdd3ed
Kellen <kellengkolbeck@gmail.com>
2015-09-07 18:11:04 -0700

    Add inital files for jQuery

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("this is a header.");
+    alert("I told you, THIS IS A HEADER!");
+  });
+
+  $('p').click(function() {
+    alert("This is a paragraph.");
+  });
+});
